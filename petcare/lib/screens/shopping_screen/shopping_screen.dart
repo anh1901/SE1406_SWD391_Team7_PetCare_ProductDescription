d1a49f7e549e5c919ae96a37cac57f13f1648cd2
DESKTOP-QJA2VUL\lmphi <lmphilip13@gmail.com>
2021-06-11 10:02:52 +0700

    Add some product for shopping screen

--- petcare/lib/screens/shopping_screen/shopping_screen.dart
+++ petcare/lib/screens/shopping_screen/shopping_screen.dart
@@ -25,25 +25,298 @@ class _ShoppingState extends State<ShoppingScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: title,
               ),
+              SizedBox(
+                  height: 20),
               Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10, left: 12),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      child: CustomText(
-                        text: "Looking for something for your pet?",
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/avatar.png',
+                          width: 50.0,
+                          height: 50.0,
+                        ),
+                      ]
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      CustomText(
+                        text: "Pet toys",
                         size: 18,
                         color: ColorStyles.color_666666,
                       ),
-                    ),
+                      CustomText(
+                        text: "Price: 30.0",
+                        size: 18,
+                        color: ColorStyles.color_666666,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          FlatButton(
+                            onPressed: (){},
+                            color: Colors.grey[400],
+                            child: Text('View Detail'),
+                            height: 30.0,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          FlatButton(
+                            onPressed: (){},
+                            color: Colors.grey[400],
+                            child: Text('Add to cart'),
+                            height: 30.0,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              SizedBox(
+                height: 5,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/avatar.png',
+                          width: 50.0,
+                          height: 50.0,
+                        ),
+                      ]
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      CustomText(
+                        text: "Pet toys",
+                        size: 18,
+                        color: ColorStyles.color_666666,
+                      ),
+                      CustomText(
+                        text: "Price: 30.0",
+                        size: 18,
+                        color: ColorStyles.color_666666,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          FlatButton(
+                            onPressed: (){},
+                            color: Colors.grey[400],
+                            child: Text('View Detail'),
+                            height: 30.0,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          FlatButton(
+                            onPressed: (){},
+                            color: Colors.grey[400],
+                            child: Text('Add to cart'),
+                            height: 30.0,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ],
               ),
               SizedBox(
                 height: 5,
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/avatar.png',
+                          width: 50.0,
+                          height: 50.0,
+                        ),
+                      ]
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      CustomText(
+                        text: "Pet toys",
+                        size: 18,
+                        color: ColorStyles.color_666666,
+                      ),
+                      CustomText(
+                        text: "Price: 30.0",
+                        size: 18,
+                        color: ColorStyles.color_666666,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          FlatButton(
+                            onPressed: (){},
+                            color: Colors.grey[400],
+                            child: Text('View Detail'),
+                            height: 30.0,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          FlatButton(
+                            onPressed: (){},
+                            color: Colors.grey[400],
+                            child: Text('Add to cart'),
+                            height: 30.0,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/avatar.png',
+                          width: 50.0,
+                          height: 50.0,
+                        ),
+                      ]
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      CustomText(
+                        text: "Pet toys",
+                        size: 18,
+                        color: ColorStyles.color_666666,
+                      ),
+                      CustomText(
+                        text: "Price: 30.0",
+                        size: 18,
+                        color: ColorStyles.color_666666,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          FlatButton(
+                            onPressed: (){},
+                            color: Colors.grey[400],
+                            child: Text('View Detail'),
+                            height: 30.0,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          FlatButton(
+                            onPressed: (){},
+                            color: Colors.grey[400],
+                            child: Text('Add to cart'),
+                            height: 30.0,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              )
             ],
           ),
         ),
